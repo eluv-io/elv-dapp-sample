fdea46ca4e8360fe60a27067f807e6bea4068c91
Todd Hodes <todd.hodes@eluv.io>
2023-04-11 20:46:07 -0700

    initial cross-chain demo conversion

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 
-# Eluvio DApp-sample
+# Eluvio DApp Sample
 
-This repository illustrates how to develop for the
-Eluvio Content Fabric CDN and Marketplace, specifically via an Eluvio
-Content Fabric crypto wallet and its associated libraries.
+## DApp Wallet Examples
+
+
+This repository illustrates how to develop for the Eluvio Content Fabric CDN and Marketplace,
+specifically via an Eluvio Content Fabric crypto wallet and its associated libraries.
 
 A version of this app is hosted for your convenience at
 [https://dapp-sample.app.eluv.io/](https://dapp-sample.app.eluv.io/).
@@ -18,9 +20,9 @@ signing, and Marketplace access. Operations highlighted by this sample:
 - User methods: calls that get info on the currently logged-in user
 - Marketplace methods: calls that get info on the marketplaces for primary and secondary sales
 - NFT ownership validation:
-  - verify ownership of an Eluvio NFT
+  - Use a cross-chain oracle query to check for ownership on an EVM blockchain (Eluvio, mainnet, polkadot, or any other)
   - Use a cross-chain oracle query to check for ownership on the flow blockchain
-  - Use a cross-chain oracle query to check for ownership on an external EVM blockchain
+  - Use a cross-chain oracle query to check for ownership on the solana blockchain
 - Embed token-gated content: simplify the embedding of fabric-hosted DRM video in an `<iframe>`
 
 The library is documented here:
@@ -44,3 +46,74 @@ To run this sample:
 The matching source code for the sample is in the `test/wallet-ops` subdirectory.  It was built atop a clone
 of the wallet client, and that code remains in the main `src/` directory for reference.
 There are other samples alongside it in the `test/` directory, feel free to run and reference them, too.
+
+
+## DApp Cross-chain app
+
+NOTES for site-sample crossover cross-chain app
+
+network: demov3
+user: starflicks-elv-admin
+site: starflicks - Properties > Site - starflicks (copy) : iq__4JsnwpGJur9VPYMjj5h59SC4Vrba
+
+objects:
+
+{
+"name": "Tears of Steel (copy)",
+"hash": "hq__8RBeZSEeZKGRucRNFDFN6Td3SgS71Yq2Lz5k4bf773HabL2B22DKxkxWGELPX2kEUQjgBG4wRc",
+"objectId": "iq__3SpYjqE2gsMkbKtxaLA1HB1Pb6Mg",
+"contract": "0xaf82ea3e4848d658daf65bc30d2ef732b31df393",
+"authorizedAsset": "eip155:955210/erc721:0x43842733179fa1c38560a44f1d9067677461c8ca"
+}
+
+{
+"name": "Meridian (copy)",
+"hash": "hq__BJ4ury6zXvHv4tG4FndgqynDR15ejEwQyeN1sojDvygqtzsfNmpkZnWLvkyfRBHBKFQoCyS53s",
+"objectId": "iq__2b7yLgWuVRZKyFXdew7kbSCu5deD",
+"contract": "0x72013e269dd5d1d0ada7441aa1c857daa9c2677e",
+"image": "https://host-76-74-28-235.contentfabric.io/q/hq__37MT83gGsJa1QSMMwauaA34ZCwWLjGXFPEsS7SQTAvr399191xjr3dGeKqYdVmvTy9GALQBUVY/meta/public/display_image?authorization=&height=328"
+"authorizedAsset": "solana:4sGjMW1sUnHzSxGspuhpqLDx6wiyjNtZ/nonfungibletoken:Ag3m1p1B6FMWKunTQwDW98fLEpcPaobmuthx1u9xLP9q"
+}
+
+{
+"name": "Caminandes - Ep 1 (copy)",
+"hash": "hq__CH4Efhpbr2sEkeFkFiLkAc9dcWCy3Ev6L4sLTusCTFDvvEPYcfzSMkqb6BUjwQTS77M8pBmM9w",
+"objectId": "iq__xGNmhvgxi6Nrc9M4kgekXPQgiiZ",
+"contract": "0x4464eb827821c819921a73b2604d6bddadb9e676",
+"authorizedAssets": [
+  "flow:mainnet/nonfungibletoken:0x329feb3ab062d289:CNN_NFT",
+  "eip155:955210/erc721:0x5755ceaa00991d223853f34a9fbc0ae0a5f683d9"
+  ]
+}
+
+{
+"name": "Caminandes - Ep 2 (copy)",
+"hash": "hq__KcVj3b2i4YCXx4CixuaDMddoYHzMwPhc9XCHBJgDMKjMdSZWAi35gojGUpQsb9ajNgGQ55Rxeq",
+"objectId": "iq__fPwqqp139xqLxApiyt6hoSgu3zm",
+"contract": "0x2f8461e1f803c10791fb6d25517431cae5a1a00e",
+"authorizedAsset": "flow:mainnet/nonfungibletoken:0x329feb3ab062d289:CNN_NFT"
+}
+
+{
+"name": "Caminandes - Ep 3 (copy)",
+"hash": "hq__KR4i87mGsixMXbZrSvDuZ5EtA4oYq9mYMrKuyr34gdKtNQU6rg4F5EXo5ETsmXr2G7qQndUoZi",
+"objectId": "iq__2QaBrtbffYaopCLGzU5tXpTHeTfn",
+"contract": "0x64f374dfe3e39641cfe9b4d122118eba70c00939",
+"authorizedAsset": "flow:mainnet/nonfungibletoken:0x329feb3ab062d289:CNN_NFT"
+}
+
+owners:
+
+eluvio: todd.hodes@eluvio.io
+- access Tears of Steel (copy) via ownership of 0x43842733179fa1c38560a44f1d9067677461c8ca
+
+metamask: 0xb516b92fe8f422555f0d04ef139c6a68fe57af08
+- owns Caminandes - Ep 1 (copy) via ownership of 0x5755ceaa00991d223853f34a9fbc0ae0a5f683d9
+
+phantom: Loren Wallet
+- owns Meridian (copy) via ownership of Ag3m1p1B6FMWKunTQwDW98fLEpcPaobmuthx1u9xLP9q
+
+flow: user 0xcbd420284fd5e19b
+- owns Caminandes - Ep 1-3 (copy) via ownership of 0x329feb3ab062d289:CNN_NFT
+
+
--- test/wallet-ops/App.js
+++ test/wallet-ops/App.js
@@ -79,9 +79,7 @@ const App = () => {
   const [inputs, setInputs] = useState(undefined);
   const [results, setResults] = useState(undefined);
   const [embed, setEmbed] = useState(undefined);
-  const [pushService, setPushService] = useState(undefined);
 
-  const clearAndShow = (results) => { setInputs(""); setEmbed(""); setResults(results); };
   const stringify = (o) => { if(typeof o === "string") { return o; } else return JSON.stringify(o, null, 2); };
   const getInput = (name) => { return document.getElementsByName(name)?.item(0)?.value || ""; };
 
@@ -110,14 +108,6 @@ const App = () => {
       </div>
     );
   }
-
-  const Sign = async () => {
-    let msgToSign = getInput("signMsg");
-    setInputs({ messageToSign: msgToSign});
-    let res = await walletClient.PersonalSign({message: msgToSign})
-      .catch(err => { return err; });
-    setResults(res);
-  };
 
   const SignSolana = async () => {
     // input is a Mint address, not Token address
@@ -167,52 +157,6 @@ const App = () => {
         window.console.error("connect with phantom first");
         setResults("<connect with phantom first>");
       }
-    }
-  };
-
-  // PushService SSE testing
-  const StartListening = async () => {
-    let count = 0;
-    const signature = await walletClient.client.CreateFabricToken({duration: 1000 * 30}).catch(err => { return err; });
-    // url for local testing
-    //const url = "http://localhost:3030/register/" +
-    const url = "https://appsvc.svc.eluv.io/push/" + (network == "main" ? "main/" : "dv3/") +
-      "register/" + walletClient.UserAddress() + "/" + signature;
-    const source = new EventSource(url);
-    window.console.log("url:", source.url, "withCreds:", source.withCredentials, "ready:", source.readyState);
-
-    source.onmessage = (event) => {
-      window.console.log("OnMessage Called[", count, "]:", event, JSON.parse(event.data));
-      count++;
-      setResults({ "notification received": JSON.parse(event.data) });
-    };
-    source.onopen = function() { window.console.log("Connection to server opened."); };
-    source.onerror = (event) => { window.console.log("OnError Called:", event); };
-
-    setPushService(source);
-  };
-
-  const StopListening = async () => {
-    window.console.log("StopListening:", pushService);
-    pushService.close();
-  };
-
-  const CheckNft = async () => {
-    const inputs = { addr: getInput("nftAddressToVerify"), ownerAddr: getInput("nftOwnerToVerify")};
-    setInputs(inputs);
-
-    let ownedOrError = await new EluvioLive(walletClient).NftBalanceOf(inputs)
-      .then(balance => {
-        return (typeof balance === "number") ? { isOwned: balance > 0, balance: balance } : { err: balance };
-      })
-      .catch(err => { return { error: err.toString()}; });
-
-    if (ownedOrError?.error) {
-      setResults(ownedOrError);
-    } else {
-      let nftStats = await walletClient.NFTContractStats({contractAddress: inputs.addr})
-        .catch(err => { return err; });
-      setResults({ ownership: ownedOrError, nftStats: nftStats });
     }
   };
 
@@ -299,11 +243,15 @@ const App = () => {
 
   return (
     <div className="page-container">
-      <h1>DApp Wallet Examples</h1>
+      <h1>Cross-chain Oracle Demo</h1>
 
-      <div className="button-row">
-        <a href="https://github.com/eluv-io/elv-dapp-sample/">https://github.com/eluv-io/elv-dapp-sample/</a>
-      </div>
+      <div><a className="source-link" href="https://github.com/eluv-io/elv-dapp-sample/" target="_blank">
+        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="-elv-icon github-icon">
+          <title>GitHub icon</title>
+          <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path>
+        </svg>Source available on GitHub
+      </a></div>
+
       <div className="button-row">
         <select value={network} onChange={ChangeNetwork}>
           <option value="main">Selected Network: main</option>
@@ -316,11 +264,6 @@ const App = () => {
       {
         walletClient.loggedIn ?
           <>
-            <div className="button-row">
-              <label htmlFor="signMsg">Sign a Message:</label>
-              <input type="text" size="50" id="signMsg" name="signMsg" />
-              <button onClick={Sign}>Sign</button>
-            </div>
             <br/>
             <div className="button-row">
               <label htmlFor="evmNft">EVM NFT chain ID:</label>
@@ -355,37 +298,7 @@ const App = () => {
               <input type="text" size="50" id="playoutVersionHash" name="playoutVersionHash" />
               <button onClick={Playout}>Embed content</button>
             </div>
-            <br />
-            <h2>User Methods</h2>
-            <div className="button-row">
-              <button onClick={async () => clearAndShow(await walletClient.UserInfo())}>UserInfo</button>
-              <button onClick={async () => clearAndShow(await walletClient.UserItems({sortBy: "default"}))}>UserItems</button>
-            </div>
-            <div className="button-row">
-              <button onClick={async () => clearAndShow(await walletClient.UserItemInfo())}>UserItemInfo</button>
-              <button onClick={async () => clearAndShow(await walletClient.AvailableMarketplaces())}>AvailableMarketplaces</button>
-            </div>
-            <div className="button-row">
-              <button onClick={async () => clearAndShow(await walletClient.client.CreateFabricToken())}>CreateFabricToken</button>
-              <button onClick={async () => clearAndShow(
-                JSON.stringify(client.utils.DecodeSignedToken(await walletClient.client.CreateFabricToken()))
-              )}>DecodeSignedToken</button>
-            </div>
-            <br />
-            <h2>Notification Service Methods</h2>
-            <div className="button-row">
-              <button onClick={async () => await StartListening()}>PushService listen</button>
-              <button onClick={async () => await StopListening()}>PushService stop</button>
-            </div>
-            <br/>
-            <h2>Marketplace Methods</h2>
-            <div className="button-row">
-              <select id="marketplaceSelector" onChange={ChangeMarketplace}/>
-            </div>
-            <div className="button-row">
-              <button onClick={async () => clearAndShow(await walletClient.Listings({marketplaceParams}))}>Listings</button>
-              <button onClick={async () => clearAndShow(await walletClient.MarketplaceStock({marketplaceParams}))}>Stock</button>
-            </div>
+            <br/><br/><br/>
           </> : null
       }
 
