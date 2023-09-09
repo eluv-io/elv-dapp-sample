49ddf4059f989b2679ed120c34d509b947f80119
Todd Hodes <todd.hodes@eluv.io>
2023-09-08 23:56:35 -0700

    do not default marketplaces

--- test/dapp-sample/App.js
+++ test/dapp-sample/App.js
@@ -37,7 +37,7 @@ const AuthSection = ({walletClient}) => {
     await walletClient.LogIn({
       method,
       callbackUrl: window.location.href,
-      marketplaceParams,
+      //marketplaceParams,
       clearLogin: true
     });
 
